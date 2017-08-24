c4c5f3648e55f7940f04b3a069fa4f5c071ab994
Kartik Redishettywar <kartikr3@citiustech.com>
2017-08-24 17:48:19 +0530

    add isLoggedIn middleware

--- routes/cognito.js
+++ routes/cognito.js
@@ -14,23 +14,26 @@ var poolData = {
 var userPool = new AWSCognito.CognitoUserPool(poolData);
 
 function isLoggedIn(req, res, next) {
-    if (req.session.cognitoUserName) {
-      var userData = {
-        Username : req.session.cognitoUserName,
-        Pool : userPool
-      };
-      var cognitoUser = new AWSCognito.CognitoUser(userData);
-      cognitoUser.getSession(function(err, session) {
-          if (err) {
-              console.log(err);
-              res.redirect('/');
-          }
-          console.log('session validity: ' + session.isValid());
-          if(session.isValid()) {
-            return next();
-          }
-      });
+    var username = req.session.cognitoUserName;
+    if (!username) {
+      res.status(400).send('You are not signed in. Sign in first.');
     }
+    var userData = {
+      Username : username,
+      Pool : userPool
+    };
+    var cognitoUser = new AWSCognito.CognitoUser(userData);
+    cognitoUser.getSession(function(err, session) {
+        if (err) {
+            res.status(400).send(err);
+        }
+        console.log('session validity: ' + session.isValid());
+        if(session.isValid()) {
+          req.session.cognito = {'idJwtToken': session.getIdToken().getJwtToken()};
+          return next();
+        }
+        res.status(400).send('Session invalid');
+    });
 }
 
 router.post('/signup', function(req, res, next) {
@@ -133,231 +136,105 @@ router.get('/signout', function(req, res, next) {
   res.send('Successfully signed out.');
 });
 
-
-/*** Upload Code start*/
-router.post('/uploadFile', function(req, res, next) {
-    if(!req.session.cognitoUserName) {
-        res.status(400).send('Session invalid');
+router.post('/uploadFile', isLoggedIn, function(req, res, next) {
+  AWS.config.region = Auth.AWS.Region;
+  AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+    IdentityPoolId: Auth.AWS.IdentityPoolId, // 'YOUR_IDENTITY_POOL_ID',
+    Logins: {
+      ['cognito-idp.' + Auth.AWS.Region + '.amazonaws.com/' + Auth.AWS.UserPoolId]: req.session.cognito.idJwtToken
     }
+  });
 
-    var userData = {
-        Username : req.session.cognitoUserName,
-        Pool : userPool
-    };
-    var cognitoUser = new AWSCognito.CognitoUser(userData);
-    cognitoUser.getSession(function(err, session) {
+  AWS.config.credentials.refresh(function() {
+      var s3 = new AWS.S3();
+      var bucket = new AWS.S3({ params: { Bucket: "rakesh-s3-bucket" } });
+      var params = { Key: req.files.file.name, ContentType: req.files.file.mimetype, Body: req.files.file.data, ServerSideEncryption: 'AES256' };
+
+      bucket.putObject(params, function(err, data) {
         if (err) {
-            console.log(err);
-            res.status(400).send(err);
+          console.log(err, err.stack); // an error occurred
+          res.status(400).send('err');
         }
-        console.log('session validity: ' + session.isValid());
+        console.log('File Uploaded Successfully', 'Done');
+        res.send(data);
+      });
+  });
+});
 
-          if(session.isValid()) {
-          //POTENTIAL: Region needs to be set if not already set previously elsewhere.
-          AWS.config.region = Auth.AWS.Region;
+router.get('/buckets', isLoggedIn, function(req, res, next) {
+  AWS.config.region = Auth.AWS.Region;
+  AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+    IdentityPoolId: Auth.AWS.IdentityPoolId, // 'YOUR_IDENTITY_POOL_ID',
+    Logins: {
+      // 'cognito-idp.<region>.amazonaws.com/<YOUR_USER_POOL_ID>': session.getIdToken().getJwtToken()
+      ['cognito-idp.' + Auth.AWS.Region + '.amazonaws.com/' + Auth.AWS.UserPoolId]: req.session.cognito.idJwtToken
+    }
+  });
 
-          // Add the User's Id Token to the Cognito credentials login map.
-          AWS.config.credentials = new AWS.CognitoIdentityCredentials({
-            IdentityPoolId: Auth.AWS.IdentityPoolId, // 'YOUR_IDENTITY_POOL_ID',
-            Logins: {
-              // 'cognito-idp.<region>.amazonaws.com/<YOUR_USER_POOL_ID>': session.getIdToken().getJwtToken()
-              ['cognito-idp.' + Auth.AWS.Region + '.amazonaws.com/' + Auth.AWS.UserPoolId]: session.getIdToken().getJwtToken()
-            }
-          });
-
-          AWS.config.credentials.refresh(function(){
-              // Your S3 code here...
-              // Instantiate aws sdk service objects now that the credentials have been updated.
-              var s3 = new AWS.S3();
-
-              var bucket = new AWS.S3({ params: { Bucket: "rakesh-s3-bucket" } });
-      //            console.log(req.files.file.name+"@@@")
-                var params = { Key: req.files.file.name, ContentType: req.files.file.mimetype, Body: req.files.file.data, ServerSideEncryption: 'AES256' };
-
-                 bucket.putObject(params, function(err, data) {
-                     if (err) {
-                         console.log(err)
-                         res.status(400).send('err');
-                         console.log(err, err.stack); // an error occurred
-                     }
-                    else {
-                      // Upload Successfully Finished
-                      console.log('File Uploaded Successfully', 'Done');
-                        res.send(data);
-                      // Reset The Progress Bar
-                   }
-               });
-
-          });
-        }
-        else {
-          res.status(400).send('Session invalid');
-        }
-
+  AWS.config.credentials.refresh(function(){
+    var s3 = new AWS.S3();
+    var params = {};
+    s3.listBuckets(params, function(err, bucketData) {
+      if(err) {
+        console.error(err);
+        res.status(400).send(err);
+      }
+      res.json(bucketData);
     });
-
-
-});
-/*** Upload Code end*/
-
-
-router.get('/buckets', function(req, res, next) {
-  // res.render('cognito/profile', {messages: messages, hasErrors: messages.length > 0});
-  if(!req.session.cognitoUserName) {
-    res.status(400).send('Session invalid');
-  }
-
-  var userData = {
-    Username : req.session.cognitoUserName,
-    Pool : userPool
-  };
-  var cognitoUser = new AWSCognito.CognitoUser(userData);
-  cognitoUser.getSession(function(err, session) {
-      if (err) {
-          console.log(err);
-          res.status(400).send(err);
-      }
-      console.log('session validity: ' + session.isValid());
-      if(session.isValid()) {
-        //POTENTIAL: Region needs to be set if not already set previously elsewhere.
-        AWS.config.region = Auth.AWS.Region;
-
-        // Add the User's Id Token to the Cognito credentials login map.
-        AWS.config.credentials = new AWS.CognitoIdentityCredentials({
-          IdentityPoolId: Auth.AWS.IdentityPoolId, // 'YOUR_IDENTITY_POOL_ID',
-          Logins: {
-            // 'cognito-idp.<region>.amazonaws.com/<YOUR_USER_POOL_ID>': session.getIdToken().getJwtToken()
-            ['cognito-idp.' + Auth.AWS.Region + '.amazonaws.com/' + Auth.AWS.UserPoolId]: session.getIdToken().getJwtToken()
-          }
-        });
-
-        AWS.config.credentials.refresh(function(){
-          // Your S3 code here...
-          // Instantiate aws sdk service objects now that the credentials have been updated.
-          var s3 = new AWS.S3();
-          var params = {};
-          s3.listBuckets(params, function(err, bucketData) {
-            if(err) {
-              console.error(err);
-              res.status(400).send(err);
-            }
-            res.json(bucketData);
-          });
-        });
-      }
-      else {
-        res.status(400).send('Session invalid');
-      }
-
   });
-
 });
 
-router.get('/buckets/bucket/:bucketName', function(req, res, next) {
-  // res.render('cognito/profile', {messages: messages, hasErrors: messages.length > 0});
-  if(!req.session.cognitoUserName) {
-    res.status(400).send('Session invalid');
-  }
+router.get('/buckets/bucket/:bucketName', isLoggedIn, function(req, res, next) {
+  AWS.config.region = Auth.AWS.Region;
 
-  var bucketName = req.params.bucketName;
-
-  var userData = {
-    Username : req.session.cognitoUserName,
-    Pool : userPool
-  };
-  var cognitoUser = new AWSCognito.CognitoUser(userData);
-  cognitoUser.getSession(function(err, session) {
-      if (err) {
-          console.log(err);
-          res.status(400).send(err);
-      }
-      console.log('session validity: ' + session.isValid());
-      if(session.isValid()) {
-        AWS.config.region = Auth.AWS.Region;
-
-        AWS.config.credentials = new AWS.CognitoIdentityCredentials({
-          IdentityPoolId: Auth.AWS.IdentityPoolId, // 'YOUR_IDENTITY_POOL_ID',
-          Logins: {
-            ['cognito-idp.' + Auth.AWS.Region + '.amazonaws.com/' + Auth.AWS.UserPoolId]: session.getIdToken().getJwtToken()
-          }
-        });
-
-        AWS.config.credentials.refresh(function() {
-          var s3 = new AWS.S3();
-          var params = {
-              Bucket: bucketName,
-              MaxKeys: 2
-          };
-          s3.listObjects(params, function(err, bucketObjects) {
-             if (err) {
-               res.status(400).send('err');
-              //  console.log(err, err.stack); // an error occurred
-             }
-             res.json(bucketObjects);
-          });
-        });
-      }
-      else {
-        res.status(400).send('Session invalid');
-      }
-
+  AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+    IdentityPoolId: Auth.AWS.IdentityPoolId, // 'YOUR_IDENTITY_POOL_ID',
+    Logins: {
+      ['cognito-idp.' + Auth.AWS.Region + '.amazonaws.com/' + Auth.AWS.UserPoolId]: req.session.cognito.idJwtToken
+    }
   });
 
+  AWS.config.credentials.refresh(function() {
+    var s3 = new AWS.S3();
+    var params = {
+        Bucket: req.params.bucketName,
+        MaxKeys: 2
+    };
+    s3.listObjects(params, function(err, bucketObjects) {
+       if (err) {
+         console.log(err, err.stack); // an error occurred
+         res.status(400).send('err');
+       }
+       res.json(bucketObjects);
+    });
+  });
 });
 
-router.get('/buckets/bucket/:bucketName/:objectKey', function(req, res, next) {
-  // res.render('cognito/profile', {messages: messages, hasErrors: messages.length > 0});
-  if(!req.session.cognitoUserName) {
-    res.status(400).send('Session invalid');
-  }
-
-  var bucketName = req.params.bucketName;
-  var objectKey = req.params.objectKey;
-
-  var userData = {
-    Username : req.session.cognitoUserName,
-    Pool : userPool
-  };
-  var cognitoUser = new AWSCognito.CognitoUser(userData);
-  cognitoUser.getSession(function(err, session) {
-      if (err) {
-          console.log(err);
-          res.status(400).send(err);
-      }
-      console.log('session validity: ' + session.isValid());
-      if(session.isValid()) {
-        AWS.config.region = Auth.AWS.Region;
-
-        AWS.config.credentials = new AWS.CognitoIdentityCredentials({
-          IdentityPoolId: Auth.AWS.IdentityPoolId, // 'YOUR_IDENTITY_POOL_ID',
-          Logins: {
-            ['cognito-idp.' + Auth.AWS.Region + '.amazonaws.com/' + Auth.AWS.UserPoolId]: session.getIdToken().getJwtToken()
-          }
-        });
-
-        AWS.config.credentials.refresh(function() {
-          var s3 = new AWS.S3();
-          var params = {
-           Bucket: bucketName,
-           Key: objectKey
-          };
-
-          s3.getObject(params, function(err, data) {
-            if (err) {
-               res.status(400).send('err'); // an error occurred
-            }
-            // res.attachment(objectKey);
-            // res.send(data.Body);
-            res.send(data);           // successful response
-          });
-        });
-      }
-      else {
-        res.status(400).send('Session invalid');
-      }
-
+router.get('/buckets/bucket/:bucketName/:objectKey', isLoggedIn, function(req, res, next) {
+  AWS.config.region = Auth.AWS.Region;
+  AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+    IdentityPoolId: Auth.AWS.IdentityPoolId,
+    Logins: {
+      ['cognito-idp.' + Auth.AWS.Region + '.amazonaws.com/' + Auth.AWS.UserPoolId]: req.session.cognito.idJwtToken
+    }
   });
 
+  AWS.config.credentials.refresh(function() {
+    var s3 = new AWS.S3();
+    var params = {
+     Bucket: req.params.bucketName,
+     Key: req.params.objectKey
+    };
+
+    s3.getObject(params, function(err, data) {
+      if (err) {
+         res.status(400).send('err'); // an error occurred
+      }
+      // res.attachment(objectKey);
+      // res.send(data.Body);
+      res.send(data);           // successful response
+    });
+  });
 });
 
 module.exports = router;
